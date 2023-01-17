f9e3b5a7a97cefb50c88aacb47e1cff15ff4e211
Michael <michailblack90@gmail.com>
2023-01-18 01:02:44 +0200

    implementing generateCellCoords.

--- src/Labyrinth/utility/RandomGenerator.h
+++ src/Labyrinth/utility/RandomGenerator.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <random>
 
+#include "Maze.h"
+
 class RandomGenerator
 {
 public:
@@ -9,9 +11,10 @@ public:
     RandomGenerator(const RandomGenerator& randomGenerator) = delete;
     RandomGenerator operator=(const RandomGenerator& randomGenerator) = delete;
 
-    static void setSeed(std::random_device::result_type seed);
+    static void setSeed(std::optional<std::random_device::result_type> seed = std::nullopt);
 
     static size_t generateIndex(size_t from, size_t to);
+    static Vec2i generateCellCoords(const Maze& maze);
 
 private:
     static std::random_device m_randDevice;
