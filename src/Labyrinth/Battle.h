e310391935ecb9300bf74c61642ca81a6f038f88
KatanaMajesty <sasha.panasenk@gmail.com>
2023-01-17 21:40:10 +0000

    Moved RobotManager to Robot.h

--- src/Labyrinth/Battle.h
+++ src/Labyrinth/Battle.h
@@ -5,32 +5,6 @@
 
 #include "Robot.h"
 
-template<typename T>
-static constexpr bool isRobot = std::is_base_of_v<IRobot, T>;
-
-class RobotManager
-{
-public:
-    RobotManager(const std::shared_ptr<Maze>& maze)
-        : m_maze(maze)
-    {
-    }
-    ~RobotManager() = default;
-
-    template<typename T>
-    std::enable_if_t<isRobot<T>, T*> AddRobot()
-    {
-        return (T*) m_robots.push_back(new T(m_maze));
-    }
-
-    inline constexpr std::vector<IRobot*>& GetRobots() { return m_robots; }
-    inline constexpr const std::vector<IRobot*>& GetRobots() const { return m_robots; }
-
-private:
-    std::vector<IRobot*> m_robots;
-    std::shared_ptr<Maze> m_maze;
-}; // RobotManager class
-
 class BattleContext
 {
 public:
@@ -75,5 +49,4 @@ private:
     std::shared_ptr<Maze> m_maze;
     bool m_closed;
     RobotManager m_robotManager;
-
 }; // Game class
